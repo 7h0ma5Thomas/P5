f67c56f078f31e9f01baf9a235314bea2d71bff1
Thomas <thomas.chaudru@gmail.com>
2022-09-13 18:54:36 +0200

    all elements added

--- front/js/product.js
+++ front/js/product.js
@@ -4,5 +4,63 @@ const id = urlParams.get ("id")
 
 fetch (`http://localhost:3000/api/products/${id}`)
     .then((response) => response.json())
-    .then((res) => console.log(res))
+    .then((res) => showImage(res))
 
+function showImage (sofa) {
+
+    const sofaImg = document.querySelector(".item__img");
+
+    const photo = document.createElement ("img");
+
+    photo.src = sofa.imageUrl;
+
+    photo.alt = sofa.altText;
+
+    sofaImg.appendChild(photo);
+
+    showName (sofa)
+}
+
+function showName (sofa) {
+
+    //const sofaName = document.querySelector("#title");
+
+    title.innerText = sofa.name;
+
+    showPrice (sofa)
+}
+
+function showPrice (sofa) {
+
+    //const sofaPrice = document.querySelector("#price");
+
+    price.innerText = sofa.price;
+
+    showDescription (sofa)
+}
+
+function showDescription (sofa) {
+
+   //const sofaDescription = document.querySelector("#description");
+
+    description.innerText = sofa.description;
+
+    chooseColor (sofa)
+}
+
+function chooseColor (sofa) {
+    for (let i=0; i < sofa.colors.length; i++){
+
+        const sofaColor = document.querySelector("#colors");
+
+        const color = document.createElement ("option");
+        
+        color.value = sofa.colors[i];
+
+        color.text = sofa.colors[i];
+
+        console.log(sofa.colors[i]);
+
+        sofaColor.appendChild(color);
+    }   
+ }
